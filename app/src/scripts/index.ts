91d109e31c557831622c5ad4013c4f13f7bed164
Shahriyor <shahriyordeveloping@gmail.com>
2021-08-27 13:49:58 +0300

    disable scroll on menu open

--- app/src/scripts/index.ts
+++ app/src/scripts/index.ts
@@ -1,13 +1,62 @@
+// scroll enable || disable
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+    get: function () { supportsPassive = true; } 
+  }));
+} catch(e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+function disableScroll() {
+  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+// call this to Enable
+function enableScroll() {
+  window.removeEventListener('DOMMouseScroll', preventDefault, false);
+  window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
+  window.removeEventListener('touchmove', preventDefault, wheelOpt);
+  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
 // Header Menu
 const headerButton: HTMLButtonElement =
   document.querySelector(".header__button");
 const headerMenu: HTMLUListElement = document.querySelector(".header__list");
 let menuOpened = false;
 const menuToggle = () => {
+  checkMenuClass()
   menuOpened = !menuOpened;
   headerButton.classList.toggle("open");
   headerMenu.classList.toggle("open");
 };
+
+function checkMenuClass(){
+  if(menuOpened == false){
+    disableScroll()
+  }else{
+    enableScroll()
+  }
+}
 
 headerButton.onclick = menuToggle;
 
